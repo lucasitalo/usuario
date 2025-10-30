d3c139268261904233914603334627885639101a
Lucas Ítalo <lucaasitaloo@gmail.com>
2025-10-30 11:32:44 -0300

    adicionado método que salva dados usuário

--- src/main/java/com/lucasitalo/usuario/business/UsuarioService.java
+++ src/main/java/com/lucasitalo/usuario/business/UsuarioService.java
@@ -1,4 +1,25 @@
 package com.lucasitalo.usuario.business;
 
+import com.lucasitalo.usuario.business.converter.UsuarioConverter;
+import com.lucasitalo.usuario.business.dto.UsuarioDTO;
+import com.lucasitalo.usuario.infrastructure.entity.Usuario;
+import com.lucasitalo.usuario.infrastructure.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+
 public class UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+    private final UsuarioConverter usuarioConverter;
+
+    public UsuarioDTO salvaUsuario(UsuarioDTO usuarioDTO){
+        Usuario usuario = usuarioConverter.paraUsuario(usuarioDTO);
+        return usuarioConverter.paraUsuarioDTO(
+                usuarioRepository.save(usuario));
+
+    }
+
 }
